d22c4b14d5f0d8209fa2a93f97757c31042a1ee5
Sjouwer <sjouwer1@gmail.com>
2022-03-11 01:37:23 +0100

    Client only

--- src/main/java/io/github/sjouwer/gammautils/GammaUtils.java
+++ src/main/java/io/github/sjouwer/gammautils/GammaUtils.java
@@ -3,23 +3,30 @@ package io.github.sjouwer.gammautils;
 import io.github.sjouwer.gammautils.config.ModConfig;
 import io.github.sjouwer.gammautils.statuseffect.*;
 import io.github.sjouwer.gammautils.util.InfoProvider;
-import net.fabricmc.api.ModInitializer;
+import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class GammaUtils implements ModInitializer {
+public class GammaUtils implements ClientModInitializer {
     public static final StatusEffect BRIGHT = new BrightStatusEffect();
     public static final StatusEffect DIM = new DimStatusEffect();
 
+    private static ConfigHolder<ModConfig> configHolder;
+
+    public static ModConfig getConfig() {
+        return configHolder.getConfig();
+    }
+
     @Override
-    public void onInitialize() {
-        AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
-        AutoConfig.getConfigHolder(ModConfig.class).registerSaveListener((manager, data) -> {
+    public void onInitializeClient() {
+        configHolder = AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
+        configHolder.registerSaveListener((manager, data) -> {
             InfoProvider.updateStatusEffect();
             return ActionResult.SUCCESS;
         });
