58abd7ac988f0c2c1aaf60030ee76aecbbd819f3
Sjouwer <sjouwer1@gmail.com>
2022-11-26 15:01:49 +0100

    Update status effects after death closes #17

--- src/main/java/io/github/sjouwer/gammautils/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/io/github/sjouwer/gammautils/mixin/MixinClientPlayNetworkHandler.java
@@ -3,6 +3,7 @@ package io.github.sjouwer.gammautils.mixin;
 import io.github.sjouwer.gammautils.statuseffect.StatusEffectManager;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.PlayerAbilitiesS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,6 +14,12 @@ public class MixinClientPlayNetworkHandler {
 
     @Inject(method = "onPlayerAbilities", at = @At("TAIL"))
     private void onPlayerAbilities(PlayerAbilitiesS2CPacket packet, CallbackInfo info) {
+        StatusEffectManager.updateGammaStatusEffect();
+        StatusEffectManager.updateNightVision();
+    }
+
+    @Inject(method = "onPlayerRespawn", at = @At("TAIL"))
+    private void onPlayerRespawn(PlayerRespawnS2CPacket packet, CallbackInfo info) {
         StatusEffectManager.updateGammaStatusEffect();
         StatusEffectManager.updateNightVision();
     }
