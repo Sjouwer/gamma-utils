6703311906de36f3d43e0fde8e6e160f5a47bc50
Sjouwer <sjouwer1@gmail.com>
2024-08-24 15:52:45 +0200

    Add command shortcuts for toggle and set
    Closes #44

--- src/main/java/io/github/sjouwer/gammautils/Commands.java
+++ src/main/java/io/github/sjouwer/gammautils/Commands.java
@@ -17,52 +17,71 @@ public class Commands {
     public static void registerGammaCommands() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
                 dispatcher.register(literal("gamma")
-                        .then(literal("toggle")
+                    .executes(ctx -> {
+                        GammaManager.toggleGamma();
+                        return 1;
+                    })
+                    .then(argument("value", integer())
                             .executes(ctx -> {
-                                GammaManager.toggleGamma();
+                                GammaManager.setGamma(getInteger(ctx, "value") / 100.0, true, true);
                                 return 1;
                             }))
-                        .then(literal("min")
+                    .then(literal("toggle")
+                        .executes(ctx -> {
+                            GammaManager.toggleGamma();
+                            return 1;
+                        }))
+                    .then(literal("min")
+                        .executes(ctx -> {
+                            GammaManager.minGamma();
+                            return 1;
+                        }))
+                    .then(literal("max")
+                        .executes(ctx -> {
+                            GammaManager.maxGamma();
+                            return 1;
+                        }))
+                    .then(literal("set")
+                        .then(argument("value", integer())
                             .executes(ctx -> {
-                                GammaManager.minGamma();
+                                GammaManager.setGamma(getInteger(ctx, "value") / 100.0, true, true);
                                 return 1;
-                            }))
-                        .then(literal("max")
+                            })))
+                    .then(literal("increase")
+                        .executes(ctx -> {
+                            GammaManager.increaseGamma(0);
+                            return 1;
+                        })
+                        .then(argument("value", integer())
                             .executes(ctx -> {
-                                GammaManager.maxGamma();
+                                GammaManager.increaseGamma(getInteger(ctx, "value") / 100.0);
                                 return 1;
-                            }))
-                        .then(literal("set")
-                            .then(argument("value", integer())
-                                .executes(ctx -> {
-                                    GammaManager.setGamma(getInteger(ctx, "value") / 100.0, true, true);
-                                    return 1;
-                                })))
-                        .then(literal("increase")
+                            })))
+                    .then(literal("decrease")
+                        .executes(ctx -> {
+                            GammaManager.decreaseGamma(0);
+                            return 1;
+                        })
+                        .then(argument("value", integer())
                             .executes(ctx -> {
-                                GammaManager.increaseGamma(0);
+                                GammaManager.decreaseGamma(getInteger(ctx, "value") / 100.0);
                                 return 1;
-                            })
-                            .then(argument("value", integer())
-                                .executes(ctx -> {
-                                    GammaManager.increaseGamma(getInteger(ctx, "value") / 100.0);
-                                    return 1;
-                                })))
-                        .then(literal("decrease")
-                            .executes(ctx -> {
-                                GammaManager.decreaseGamma(0);
-                                return 1;
-                            })
-                            .then(argument("value", integer())
-                                .executes(ctx -> {
-                                    GammaManager.decreaseGamma(getInteger(ctx, "value") / 100.0);
-                                    return 1;
-                                })))));
+                            })))));
     }
 
     public static void registerNightVisionCommands() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
                 dispatcher.register(literal("nightvision")
+                    .executes(ctx -> {
+                        NightVisionManager.toggleNightVision();
+                        return 1;
+                    })
+                    .then(argument("value", integer())
+                            .executes(ctx -> {
+                                NightVisionManager.toggleNightVision();
+                                NightVisionManager.setNightVision(getInteger(ctx, "value"), true, false, true);
+                                return 1;
+                            }))
                     .then(literal("toggle")
                         .executes(ctx -> {
                             NightVisionManager.toggleNightVision();
@@ -71,6 +90,7 @@ public class Commands {
                     .then(literal("set")
                         .then(argument("value", integer())
                             .executes(ctx -> {
+                                NightVisionManager.toggleNightVision();
                                 NightVisionManager.setNightVision(getInteger(ctx, "value"), true, false, true);
                                 return 1;
                             })))
