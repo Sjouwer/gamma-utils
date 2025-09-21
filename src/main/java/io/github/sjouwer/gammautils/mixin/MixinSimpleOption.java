855bfcec86ae913f7a7265ddd73dbffed227c56b
Sjouwer <sjouwer1@gmail.com>
2025-09-21 17:48:38 +0200

    Peformance improvements
    resolves #78

--- src/main/java/io/github/sjouwer/gammautils/mixin/MixinSimpleOption.java
+++ src/main/java/io/github/sjouwer/gammautils/mixin/MixinSimpleOption.java
@@ -2,11 +2,12 @@ package io.github.sjouwer.gammautils.mixin;
 
 import io.github.sjouwer.gammautils.GammaUtils;
 import net.minecraft.client.option.SimpleOption;
-import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,7 +24,7 @@ public class MixinSimpleOption<T> {
      */
     @Inject(method = "getValue", at = @At("HEAD"), cancellable = true)
     public void getModValue(CallbackInfoReturnable<Double> info) {
-        if (text.getString().equals(I18n.translate("options.gamma"))) {
+        if (isGammaOption()) {
             info.setReturnValue(GammaUtils.getConfig().gamma.getValue());
         }
     }
@@ -32,10 +33,19 @@ public class MixinSimpleOption<T> {
      * Mixin to set the gamma value of this mod instead of the vanilla one
      */
     @Inject(method = "setValue", at = @At("HEAD"), cancellable = true)
-    private void setModValue(T value, CallbackInfo info) {
-        if (text.getString().equals(I18n.translate("options.gamma"))) {
+    public void setModValue(T value, CallbackInfo info) {
+        if (isGammaOption()) {
             GammaUtils.getConfig().gamma.setValue((Double) value);
             info.cancel();
         }
+    }
+
+    @Unique
+    private boolean isGammaOption() {
+        if (text.getContent() instanceof TranslatableTextContent translatableTextContent) {
+            return translatableTextContent.getKey().equals("options.gamma");
+        }
+
+        return false;
     }
 }
