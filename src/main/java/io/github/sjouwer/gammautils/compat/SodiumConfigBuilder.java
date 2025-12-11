2efe8ddada8408f14c6bcde8a455463d103b800d
Sjouwer <sjouwer1@gmail.com>
2025-12-11 20:50:56 +0100

    Fix compatibility with Sodium 0.8

---
+++ src/main/java/io/github/sjouwer/gammautils/compat/SodiumConfigBuilder.java
@@ -0,0 +1,33 @@
+package io.github.sjouwer.gammautils.compat;
+
+import io.github.sjouwer.gammautils.GammaUtils;
+import io.github.sjouwer.gammautils.config.ModConfig;
+import net.caffeinemc.mods.sodium.api.config.ConfigEntryPoint;
+import net.caffeinemc.mods.sodium.api.config.structure.ConfigBuilder;
+import net.minecraft.util.Identifier;
+
+public class SodiumConfigBuilder implements ConfigEntryPoint {
+    @Override
+    public void registerConfigLate(ConfigBuilder builder) {
+        ModConfig.GammaSettings gamma = GammaUtils.getConfig().gamma;
+
+        int min = gamma.getMin();
+        int max = gamma.getMax();
+        if (min > max) {
+            min = -750;
+            max = 1500;
+        }
+
+        int step = gamma.getStep();
+        if (min % step != 0 || max % step != 0) {
+            step = 1;
+        }
+
+        builder.registerModOptions(GammaUtils.NAMESPACE, "Gamma Utils", "1")
+                .registerOptionOverlay(
+                        Identifier.of("sodium:general.gamma"),
+                        builder.createIntegerOption(
+                                Identifier.of("sodium:general.gamma")).setRange(min, max, step)
+                );
+    }
+}
--- src/main/java/io/github/sjouwer/gammautils/config/ModMenuApiImpl.java
+++ src/main/java/io/github/sjouwer/gammautils/config/ModMenuApiImpl.java
@@ -2,7 +2,7 @@ package io.github.sjouwer.gammautils.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
 
@@ -11,6 +11,6 @@ public class ModMenuApiImpl implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
     }
 }
