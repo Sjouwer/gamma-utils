0b64e2c9da5975b175d5b75add9a85f4bdc7bfd8
Sjouwer <sjouwer1@gmail.com>
2026-07-26 18:03:49 +0200

    Add config file precheck to resolve AutoConfig crash
    Resolves https://github.com/shedaniel/cloth-config/issues/339 on Gamma Utils side instead of the source, as proper fix on Cloth Config side hasn't been implemented yet.

--- src/main/java/io/github/sjouwer/gammautils/GammaUtils.java
+++ src/main/java/io/github/sjouwer/gammautils/GammaUtils.java
@@ -15,11 +15,16 @@ import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Mod(value = GammaUtils.NAMESPACE, dist = Dist.CLIENT)
 @EventBusSubscriber(modid = GammaUtils.NAMESPACE, value = Dist.CLIENT)
@@ -40,11 +45,7 @@ public class GammaUtils {
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        configHolder = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
-        configHolder.registerSaveListener((manager, data) -> {
-            StatusEffectManager.updateAllEffects();
-            return InteractionResult.SUCCESS;
-        });
+        loadConfigFile();
 
         ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () ->
                 (_, parent) -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get());
@@ -70,5 +71,37 @@ public class GammaUtils {
     public static void onPreClientTick(ClientTickEvent.Pre event) {
         GammaManager.setDynamicGamma();
         NightVisionManager.setDynamicNightVision();
+    }
+
+    private static void loadConfigFile() {
+        try {
+            precheckConfigFile();
+            configHolder = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+        }
+        catch (Exception e) {
+            // Extra message to help the user in case the precheck didn't work.
+            LOGGER.error("Failed to load gammautils config file, try manually deleting the file to resolve this issue. Report this issue on the Gamma Utils repository if the game continues to crash.");
+            throw e;
+        }
+
+        configHolder.registerSaveListener((manager, data) -> {
+            StatusEffectManager.updateAllEffects();
+            return InteractionResult.SUCCESS;
+        });
+    }
+
+    // In rare cases the config file might be completely empty, which causes Cloth Config to crash. This should resolve that issue.
+    private static void precheckConfigFile() {
+        Path configFolder = FMLPaths.CONFIGDIR.get();
+        Path configFile = configFolder.resolve("gammautils.json");
+
+        try {
+            if (Files.exists(configFile) && Files.size(configFile) == 0) {
+                LOGGER.warn("Config file is empty, deleting it so a new one can be created by Cloth Config");
+                Files.delete(configFile);
+            }
+        } catch (IOException _) {
+            LOGGER.warn("Failed precheck of config file");
+        }
     }
 }
